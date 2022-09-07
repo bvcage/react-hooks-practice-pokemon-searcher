e371dabfca6d85fceed84f4b3a17605634c5bc35
Bailey Cage <bailey.cage@gmail.com>
2022-09-07 10:59:19 -0400

    toggle front/back sprite on click

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
 function PokemonCard({ pokemon }) {
 
   const { id, name, hp, sprites } = pokemon;
+  const [displayImg, setDisplayImg] = useState('front');
+  
+  function toggleSprite () {
+    if (displayImg === 'front') setDisplayImg('back');
+    else setDisplayImg('front');
+  }
 
   return (
-    <Card>
+    <Card onClick={toggleSprite}>
       <div>
         <div className="image">
-          <img src={sprites.front} alt={name} />
+          <img src={sprites[displayImg]} alt={name} />
         </div>
         <div className="content">
           <div className="header">{name}</div>
