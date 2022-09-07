9ba21a1cf238147cbfabe9da1abfaf0647d05537
Bailey Cage <bailey.cage@gmail.com>
2022-09-07 12:00:57 -0400

    make PokemonForm into controlled components

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "semantic-ui-react";
 
 function PokemonForm({ createNewPoke }) {
 
+  const initialPoke = {
+    name: '',
+    hp: 0,
+    sprites: {
+      front: '',
+      back: '',
+    },
+  }
+  
+  const [newPoke, setNewPoke] = useState(initialPoke);
+
+  function handleChange (event) {
+    let inputKey = event.target.name;
+    let inputValue = event.target.value;
+    switch (inputKey) {
+      case 'frontUrl':
+      case 'backUrl':
+        let spriteKey = inputKey.slice(0, -3);
+        setNewPoke({...newPoke,
+          sprites: {...newPoke.sprites,
+            [spriteKey]: inputValue,
+          }
+        })
+        break;
+      default:
+        setNewPoke({...newPoke,
+          [inputKey]: inputValue,
+        })
+        break;
+    }
+  }
+
   function handleSubmit (event) {
     event.preventDefault();
-    const formData = event.target;
-
     console.log('submitting ...');
-    
-    const newPoke = {
-      name: formData.name.value,
-      hp: formData.hp.value,
-      sprites: {
-        front: formData.frontUrl.value,
-        back: formData.backUrl.value,
-      },
-    }
-    
     createNewPoke(newPoke);
+    resetNewPoke();
+  }
+
+  function resetNewPoke () {
+    setNewPoke(initialPoke);
   }
 
   return (
@@ -28,19 +53,37 @@ function PokemonForm({ createNewPoke }) {
         onSubmit={handleSubmit}
       >
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
+          <Form.Input
+            fluid
+            label="Name"
+            placeholder="Name"
+            name="name"
+            value={newPoke.name}
+            onChange={handleChange}
+          />
+          <Form.Input
+            fluid
+            label="hp"
+            placeholder="hp"
+            name="hp"
+            value={newPoke.hp}
+            onChange={handleChange}
+          />
           <Form.Input
             fluid
             label="Front Image URL"
             placeholder="url"
             name="frontUrl"
+            value={newPoke.sprites.front}
+            onChange={handleChange}
           />
           <Form.Input
             fluid
             label="Back Image URL"
             placeholder="url"
             name="backUrl"
+            value={newPoke.sprites.back}
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Button>Submit</Form.Button>
