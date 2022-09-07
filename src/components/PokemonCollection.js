989d0e47ec40db4ad799dc54a18893947e47493f
Bailey Cage <bailey.cage@gmail.com>
2022-09-07 10:50:37 -0400

    fetch & display pokemon cards for collection

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
 function PokemonCollection() {
+
+  const [pokeAry, setPokeAry] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/pokemon`)
+    .then(r => r.json())
+    .then(data => setPokeAry(data));
+  },[]);
+
+  const pokeCards = pokeAry.map(pokemon => {
+    return (
+      <PokemonCard key={pokemon.id} pokemon={pokemon} />
+    )
+  })
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      <>{pokeCards}</>
     </Card.Group>
   );
 }
