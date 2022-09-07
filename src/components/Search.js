6a606d715a3521b6b275d044d4891dd24ee25e8a
Bailey Cage <bailey.cage@gmail.com>
2022-09-07 11:12:58 -0400

    remove unneeded function from Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,10 @@
 import React from "react";
 
 function Search({ searchVal, onChange }) {
-
-  function handleInput (event) {
-    onChange(event);
-  }
-
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" value={searchVal} onChange={handleInput} />
+        <input className="prompt" value={searchVal} onChange={onChange} />
         <i className="search icon" />
       </div>
     </div>
