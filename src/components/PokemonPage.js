17272ed47f30bcd8aebc1175283fc83955e5f653
Bailey Cage <bailey.cage@gmail.com>
2022-09-07 11:04:21 -0400

    make search input controlled component

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+
+  const [searchVal, setSearchVal] = useState('');
+
+  function handleSearch (event) {
+    setSearchVal(event.target.value);
+  }
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search searchVal={searchVal} onChange={handleSearch} />
       <br />
       <PokemonCollection />
     </Container>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Search() {
+function Search({ searchVal, onChange }) {
+
+  function handleInput (event) {
+    onChange(event);
+  }
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" />
+        <input className="prompt" value={searchVal} onChange={handleInput} />
         <i className="search icon" />
       </div>
     </div>
